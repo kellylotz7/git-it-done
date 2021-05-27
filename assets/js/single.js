4e9dd9e9d73fe80ae29b6385fec0bf00676e87f9
Kelly Lotz <kellylotz7@gmail.com>
2021-05-27 14:54:22 -0600

    added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -18,7 +18,8 @@ var getRepoIssues = function(repo) {
           });
         }
         else {
-          alert("There was a problem with your request!");
+          // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
       });
   };
@@ -26,8 +27,15 @@ var getRepoIssues = function(repo) {
   var getRepoName = function(){
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
+
+    if(repoName) {
+      repoNameEl.textContent = repoName;
+      getRepoIssues(repoName);
+    }
+
+    else {
+      document.location.replace("./index.html");
+    }
   }
 
   var displayWarning = function(repo) {
