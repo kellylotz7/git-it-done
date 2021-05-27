af5fc0a79f769c230eaa84220f8c9dd6c85659a3
Kelly Lotz <kellylotz7@gmail.com>
2021-05-27 14:45:58 -0600

    single-repo.html dynamically rendering issues based on repo selected

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -21,6 +22,13 @@ var getRepoIssues = function(repo) {
         }
       });
   };
+
+  var getRepoName = function(){
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+  }
 
   var displayWarning = function(repo) {
     // add text to warning container
@@ -69,4 +77,4 @@ var getRepoIssues = function(repo) {
     }
 };
   
-  getRepoIssues("facebook/react");
+getRepoName();
